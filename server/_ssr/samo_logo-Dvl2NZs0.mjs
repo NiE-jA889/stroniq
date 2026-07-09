b1607b0e9f757c8aea393869b5484075ea5195e2
NiE-jA889 <aleksanderdymek0@gmail.com>
2026-07-09 22:35:42 +0200

    Delete server directory

--- server/_ssr/samo_logo-Dvl2NZs0.mjs
+++
@@ -1,4 +0,0 @@
-//#region node_modules/.nitro/vite/services/ssr/assets/samo_logo-Dvl2NZs0.js
-var samo_logo_default = "/assets/samo_logo-B7Rv1VP5.png";
-//#endregion
-export { samo_logo_default as t };
